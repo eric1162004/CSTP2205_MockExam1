190ef4615cf8093820a1e4554f66956b205645e7
Eric Cheung <eric1162004@hotmail.com>
2021-11-25 20:44:41 -0800

    Initial commit

---
+++ app/src/main/java/com/example/mock1exam/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.mock1exam.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(246, 214, 35)
+val SecondaryColor = Color(251, 198, 11)
+val ThirdColor = Color(243, 110, 0)
+
+val OverlayScreenBackgroundColor = Color(0f, 0f, 0f, 0.5f)
+val ErrorColor = Color(0xffF44336)
+
+
+
+
